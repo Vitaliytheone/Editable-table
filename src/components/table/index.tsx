a4032fd3804c17dd3bf9d7a112e62a9f535ec469
VITALIJ <Vitaliythesecond@gmail.com>
2022-04-05 23:48:56 +0300

    new table

--- src/components/table/index.tsx
+++ src/components/table/index.tsx
@@ -1,26 +1,33 @@
 import { scheme } from "./sheme";
 import { TTable } from "./type";
+import "./style.scss";
 
 const Table = ({}: TTable) => {
     return (
         <table>
             <thead>
                 <tr>
-                    {scheme.map(({ id, name, width }) => (
-                        <th key={id} style={{ width }}>
-                            <div>{name}</div>
-                        </th>
-                    ))}
+                    <th scope="col">Name</th>
+                    <th scope="col">Surname</th>
+                    <th scope="col">Address</th>
+                    <th scope="col">Age</th>
+                    <th scope="col">Avg.mark</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                    {scheme.map(({ id, name }) => (
-                        <td key={id} data-label={name}>
-                            123
-                            {/* {getValue(item[id])} */}
-                        </td>
-                    ))}
+                    <td data-label="Name">Vitaliy</td>
+                    <td data-label="Surname">Shevchenko</td>
+                    <td data-label="Address">Sevastopol</td>
+                    <td data-label="Age">28</td>
+                    <td data-label="Avg.mark">4.5</td>
+                </tr>
+                <tr>
+                    <td data-label="Name">Vitaliy</td>
+                    <td data-label="Surname">Shevchenko</td>
+                    <td data-label="Address">Sevastopol</td>
+                    <td data-label="Age">28</td>
+                    <td data-label="Avg.mark">4.5</td>
                 </tr>
             </tbody>
         </table>
